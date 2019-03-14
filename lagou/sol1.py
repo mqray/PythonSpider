8046f7d6f66eef9ec5d2e765162fdfe8cf88bf97
mqray <mqray@users.noreply.github.com>
2019-03-14 21:05:08 +0800

    update something new.

--- lagou/sol1.py
+++ lagou/sol1.py
@@ -10,10 +10,9 @@ import re
 from pymongo import MongoClient
 # from pyquery import PyQuery as pq
 from requests.exceptions import RequestException,ConnectionError
-def get_page_list(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-    }
+import  time
+def get_page_list(url,headers):
+    headers = headers
     sess = requests.Session()
     response = requests.get(url,headers=headers)
     cookie = sess.cookies
@@ -29,12 +28,10 @@ def get_page_list(url):
         print('RequestionError')
         return None
 
-def get_infos(url,cookie):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
-    }
+def get_infos(url,cookie,headers):
+    headers = headers
     try:
-        response = requests.get(url, cookies=cookie,headers=headers)
+        response = requests.get(url,headers=headers,cookies=cookie)
         items = {}
         if response.status_code == 200:
             html = etree.HTML(response.text)
@@ -42,14 +39,19 @@ def get_infos(url,cookie):
             # print(len(job_name))
             items['name'] = html.xpath('//div[@class="job-name"]/span[@class="name"]/text()')
             items['company'] = html.xpath('//div[@class="job-name"]/div[@class="company"]/text()')
-            job_request = html.xpath('//div[@class="position-content-l"]/dd[@class="job_request"]')[0]
-
-            items['salary'] = job_request.xpath('./p/span[1]/text()')
-            items['loc'] = job_request.xpath('./p/span[2]/text()')
-            items['work_exp'] = job_request.xpath('./p/span[3]/text()')
-            items['education'] = job_request.xpath('./p/span[4]/text()')
-            items['job_type'] = job_request.xpath('./p/span[last()]/text()')
-            items['punish_time'] = job_request.xpath('.//p[@class="publish_time"]/text()')
+            job_request = html.xpath('//div[@class="position-content-l"]/dd/p[1]/span/text()')
+            # job_request = html.xpath('//div[@class="position-content-l"]/dd/p[1]')
+            # items['salary'] = job_request.xpath('./span[1]/text()')
+            # items['loc'] = job_request.xpath('./span[2]/text()')
+            # items['work_exp'] = job_request.xpath('./span[3]/text()')
+            # items['education'] = job_request.xpath('./span[4]/text()')
+            # items['job_type'] = job_request.xpath('./span[last()]/text()')
+            items['salary'] = job_request[0]
+            items['loc'] = job_request[1]
+            items['work_exp'] = job_request[2]
+            items['education'] = job_request[3]
+            items['job_type'] = job_request[4]
+            items['punish_time'] = html.xpath('//div[@class="position-content-l"]/dd/p[2]/text()')
             #content
             content = html.xpath('//dd[@class="job_bt"]/div[@class="job-detail"]//text()')
             # items['description'] = re.match(r'(.*?)职位要求',content)
@@ -61,31 +63,46 @@ def get_infos(url,cookie):
     except ConnectionError:
         print('ConnectionError')
 
-# def regular_data(dic):
-#     print(type(items))
-#     for k in dic.items() :
-#         dic[k] = "".join(dic[k].spilt())
-#     return dic
-#
-# def save_to_mongo(dic):
-#     conn = MongoClient('localhost')
-#     db = conn.LG
-#     my_set = db.file1
-#     my_set.insert(dict)
-#     print('succ to mongo ')
+def regular_data(dic):
+    for key in dic:
+        if isinstance(dic[key], list):
+            dic[key] = [re.sub("\xa0|/|-|\n|/s| ", "", i) for i in dic[key]]
+            dic[key] = [i for i in dic[key] if len(i) > 0]
+        else:
+            dic[key] = re.sub("\xa0|/|-|\n|/s| ", "", dic[key])
+    # for key in dic :
+    #     # dic[k] = "".join(dic[k].spilt())
+    #     dic[key] = re.sub("/|/xao|/s|/n", "", dic[key])
+    return dic
+
+def save_to_mongo(dic):
+    client = MongoClient('localhost')#创建连接
+    db = client['lg']#连接的数据库
+    my_table = db['sol1']#连接的表
+    my_table.insert_many(dic)
+    print('succ to mongo ')
 
 def main():
-    for i in range(1,30):
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-GB;q=0.8,en-US;q=0.7,en;q=0.6,zh-TW;q=0.5',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive',
+        'Host': 'www.lagou.com',
+        'Upgrade-Insecure-Requests': '1',
+        'Referer': 'https://www.lagou.com/',
+        'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko)  Safari/537.36)'
+    }
+    for i in range(1,31):
         ori_url = 'https://www.lagou.com/zhaopin/Python/'+ str(i)
-        page_lists,cookie =  get_page_list(ori_url)
+        page_lists,cookie=  get_page_list(ori_url, headers=headers)
+        print(page_lists)
         for page_list in page_lists:
-            print(page_list)
-            infos = get_infos(page_list,cookie)
-            print(type(infos))
-            # infos = regular_data(infos)
-            # save_to_mongo(infos)
-
-
+            infos = get_infos(page_list,cookie,headers)
+            #print(type(infos))
+            infos = regular_data(infos)
+            save_to_mongo(infos)
 
 if __name__ == '__main__':
     main()
--- lagou/test2.py
+++ lagou/test2.py
@@ -5,6 +5,43 @@
 # @File    : test2.py
 # @Software: PyCharm
 import  requests
-url = 'https://www.lagou.com/jobs/5682511.html'
-resp = requests.get(url)
-print(resp.text)
+from lxml import etree
+url = 'https://www.lagou.com/jobs/5685848.html'
+headers ={
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-GB;q=0.8,en-US;q=0.7,en;q=0.6,zh-TW;q=0.5',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'www.lagou.com',
+    'Upgrade-Insecure-Requests': '1',
+    'Referer': 'https://www.lagou.com/',
+    'User-Agent': 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko)  Safari/537.36)'
+}
+response = requests.get(url,headers=headers)
+print(response.text)
+# print(response.request.headers)
+# print(response.text)
+html = etree.HTML(response.text)
+
+job_request = html.xpath('//div[@class="position-content-l"]/dd/p[1]/span/text()')
+name = html.xpath('//div[@class="position-content-l"]')
+
+print(name)
+
+
+# 清洗抓取到的数据
+# import  re
+# dic = { 'name':'Python工程师','loc':'前海农交所招聘','job_type': '全职', 'work_addr': ['\n                                                ', ' -\n                    ', '\n                                            - 天安科技园总部大厦19号楼伊的家总部大厦\n                                                            ', '\n        ']}
+# #TypeError: expected string or bytes-like object
+# for key in dic:
+#     if isinstance(dic[key],list):
+#         dic[key] = [re.sub("\xa0|-|\n|/s| ","",i) for i in dic[key] ]
+#         dic[key] = [i for i in dic[key] if len(i)>0]
+#         print(dic[key],type(dic[key]))
+#     else :
+#         dic[key] = re.sub("\xa0|-|\n|/s| ","",dic[key])
+#         print(dic[key],type(dic[key]))
+
+
+
